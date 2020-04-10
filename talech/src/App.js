9c864690d3abeef18c587bc03958f5c3e0df7375
Linas Suchockas <linas.suchockas@gmail.com>
2020-04-10 14:10:53 +0300

    Adding the buttons and seeing the view of the table

--- talech/src/App.js
+++ talech/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App()
+{
     return (
         <div className="App">
             <header className = "App_header">
                 <h1>Talech Task</h1>
             </header>
             <div>
-                <Product_Fields />
+                <Table />
             </div>
         </div>
     );
@@ -16,8 +17,9 @@ function App() {
 
 export default App;
 
-class Product_Fields extends React.Component {
-    render() {
+class Table extends React.Component {
+    render()
+    {
         return (
             <div>
                 <table className="App_table">
@@ -25,13 +27,55 @@ class Product_Fields extends React.Component {
                         <th>Name</th>
                         <th>EAN</th>
                         <th>Type</th>
-                        <th>Wight(g)</th>
+                        <th>Weight (g)</th>
                         <th>Color</th>
                         <th>Active</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th>Sugar</th>
+                        <th>555</th>
+                        <th>Sweet</th>
+                        <th>100</th>
+                        <th>White</th>
+                        <th> <CheckBox /> </th>
+                        <th> <Button_view /> <Button_edit /> <Button_delete /></th>
                     </tr>
                 </table>
             </div>
-            
+        );
+    }
+}
+class Button_view extends React.Component {
+    render() {
+        return (
+            <input type="button" value="VIEW" />
+        );
+    }
+}
+class Button_edit extends React.Component {
+    render() {
+        return (
+            <input type="button" value="EDIT" />
+        );
+    }
+}
+class Button_delete extends React.Component
+{
+    render()
+    {
+        return (
+            <input type="button" value="DELETE"/> 
+        );
+    }
+}
+
+class CheckBox extends React.Component 
+{
+    render() 
+    {
+        return (
+            <input name="Active" type="checkbox" />
         );
     }
 }
